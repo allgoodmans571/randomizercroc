0bea5273ed68b74ff6eb788b0b0e24387ee29079
Alex <allgoodmans571@gmail.com>
2020-11-20 13:28:45 +0300

    modal forms done

--- src/Modal/DeliverModal.js
+++ src/Modal/DeliverModal.js
@@ -1,3 +1,4 @@
+
 import React, {Fragment} from 'react'
 import './Modal.css'
 
@@ -14,13 +15,18 @@ export default class DeliverModal extends React.Component{
                 <button className='modBtn' onClick={() => this.setState({isOpen: true})} > Доставка </button>
                 {this.state.isOpen && <div className='modal'>
                     <div className='modal-body'>
-                        <form className='SelfDeliverForm'>
-                          <h1 className="text">Кто будет забирать </h1> <br />
-                            <input type='text' className='modInput' /><br />
-                            <h1 className="text">Что будет забирать </h1> <br />
-                            <input type='text' className='modInput' /><br />
-                            <h1 className="text">Дата самовывоза</h1> <br />
-                            <input type='text' className='modInput' /><br />
+                        <h1 className='modHead' >Доставка</h1>
+                        <form className='DeliverForm'>
+                          <h1 className="text">Кому доставить </h1> <br />
+                            <input type='text' className='modInput' placeholder='ФИО' /><br />
+                            <h1 className="text">Что доставить </h1> <br />
+                            <input type='text' className='modInput' placeholder='Ручка с лого КРОК' /><br />
+                            <h1 className="text">Дата доставки</h1> <br />
+                            <input type='text' className='modInput' placeholder='12 ноября 2020, 15:15' /><br />
+                            <h1 className="text">Куда доставить</h1> <br />
+                            <input type='text' className='modInput' placeholder='Адрес' /><br />
+                            <h1 className="text">Куда звонить</h1> <br />
+                            <input type='text' className='modInput' placeholder='+7'/><br />
                         </form>
                         <div className='acceptBtn'>
                         <button className='accept' onClick={() => this.setState({isOpen: false})}>Принять</button>
--- src/Modal/SelfDeliverModal.js
+++ src/Modal/SelfDeliverModal.js
@@ -11,20 +11,17 @@ export default class SelfDeliverModal extends React.Component{
     render() {
         return (
             <Fragment>
-                <button className='modBtn' onClick={() => this.setState({isOpen: true})} > Доставка </button>
+                <button className='modBtn' onClick={() => this.setState({isOpen: true})} > Самовывоз </button>
                 {this.state.isOpen && <div className='modal'>
                     <div className='modal-body'>
+                        <h1 className='modHead' >Самовывоз</h1>
                         <form className='SelfDeliverForm'>
-                          <h1 className="text">Кому доставить </h1> <br />
-                            <input type='text' className='modInput' /><br />
-                            <h1 className="text">Что доставить </h1> <br />
-                            <input type='text' className='modInput' /><br />
-                            <h1 className="text">Дата доставки</h1> <br />
-                            <input type='text' className='modInput' /><br />
-                            <h1 className="text">Куда доставить</h1> <br />
-                            <input type='text' className='modInput' /><br />
-                            <h1 className="text">Куда звонить</h1> <br />
-                            <input type='text' className='modInput' /><br />
+                          <h1 className="text">Кто будет забирать </h1> <br />
+                            <input type='text' className='modInput' placeholder='ФИО' /><br />
+                            <h1 className="text">Что будет забирать </h1> <br />
+                            <input type='text' className='modInput' placeholder='{ItemName}' /><br />
+                            <h1 className="text">Дата самовывоза</h1> <br />
+                            <input type='text' className='modInput' placeholder='12 ноября 2020, 15:15' /><br />
                         </form>
                         <div className='acceptBtn'>
                         <button className='accept' onClick={() => this.setState({isOpen: false})}>Принять</button>
